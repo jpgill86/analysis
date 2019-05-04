1883db1e176d8af68d67aa2476f0ad9fc19f4083
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2019-05-03 22:27:41 -0400

    Move fully generalized behavior/subepoch locating function to module
    The feeding conditions notebook now takes advantage of the new `BehaviorsDataFrame` function. Some other notebooks could be updated to do the same.

--- modules/NeoUtilities.py
+++ modules/NeoUtilities.py
@@ -79,3 +79,51 @@ def NeoEpochToDataFrame(neo_epochs, exclude_epoch_encoder_epochs=False):
             data = np.array([ep.times, ep.times+ep.durations, ep.durations, [ep.name]*len(ep), ep.labels]).T
             df = df.append(pd.DataFrame(data, columns=columns), ignore_index=True)
     return df.astype(dtype=dtypes).sort_values(['Start (s)', 'End (s)', 'Type', 'Label']).reset_index(drop=True)
+
+def BehaviorsDataFrame(neo_epochs, behavior_query, subepoch_queries):
+    '''
+
+    '''
+
+    # filter epochs to obtain the behaviors
+    df = NeoEpochToDataFrame(neo_epochs)
+    df = df.rename(columns={'Start (s)': 'Start', 'End (s)': 'End', 'Duration (s)': 'Duration'})
+    df = df.query(behavior_query)
+    df = df.rename(columns={'Start': 'Start (s)', 'End': 'End (s)', 'Duration': 'Duration (s)'})
+
+    # add defaults for subepoch columns
+    for col_prefix in subepoch_queries:
+        df[col_prefix+' start (s)'] = np.nan
+        df[col_prefix+' end (s)'] = np.nan
+        df[col_prefix+' duration (s)'] = np.nan
+        df[col_prefix+' type'] = ''
+        df[col_prefix+' label'] = ''
+
+    # for every behavior, identify other epochs that belong to it
+    df2 = NeoEpochToDataFrame(neo_epochs)
+    df2 = df2.rename(columns={'Start (s)': 'Start', 'End (s)': 'End', 'Duration (s)': 'Duration'})
+    for i in df.index:
+
+        # these variables are accessible within queries when the
+        # @ symbol appears before their names, e.g. @behavior_start
+        behavior_start    = df.loc[i, 'Start (s)']
+        behavior_end      = df.loc[i, 'End (s)']
+        behavior_duration = df.loc[i, 'Duration (s)']
+        behavior_type     = df.loc[i, 'Type']
+        behavior_label    = df.loc[i, 'Label']
+
+        for col_prefix, query in subepoch_queries.items():
+            df3 = df2.query(query)
+            if len(df3) == 0:
+                pass
+            elif len(df3) == 1:
+                matching_epoch = df3.iloc[0]
+                df.loc[i, col_prefix+' start (s)'] = matching_epoch['Start']
+                df.loc[i, col_prefix+' end (s)'] = matching_epoch['End']
+                df.loc[i, col_prefix+' duration (s)'] = matching_epoch['Duration']
+                df.loc[i, col_prefix+' type'] = matching_epoch['Type']
+                df.loc[i, col_prefix+' label'] = matching_epoch['Label']
+            else:
+                raise Exception(f'More than one epoch was found for the behavior spanning [{behavior_start}, {behavior_end}] that matches this query: {query}')
+
+    return df
