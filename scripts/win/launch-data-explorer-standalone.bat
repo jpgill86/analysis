d4baf95f8bcd1b4123f1219fd48e65e10106c526
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2019-06-09 03:31:51 -0400

    Add system-wide install paths to Windows scripts

--- scripts/win/launch-data-explorer-standalone.bat
+++ scripts/win/launch-data-explorer-standalone.bat
@@ -9,11 +9,15 @@ IF EXIST %USERPROFILE%\Anaconda3 (
 ) ELSE (
 IF EXIST %USERPROFILE%\Miniconda3 (
   SET CONDAROOT=%USERPROFILE%\Miniconda3
+IF EXIST C:\ProgramData\Anaconda3 (
+  SET CONDAROOT=C:\ProgramData\Anaconda3
+IF EXIST C:\ProgramData\Miniconda3 (
+  SET CONDAROOT=C:\ProgramData\Miniconda3
 ) ELSE (
   ECHO ERROR: Could not find the conda root directory!
   PAUSE
   EXIT
-))
+))))
 
 CALL "%CONDAROOT%\Scripts\activate" "%CONDAENV%"
 
