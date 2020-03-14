65eea23b195d65e8905d37a2d258365a8e215a7f
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2020-03-14 17:20:35 -0400

    Import CausalAlphaKernel from neurotic into utils
    CausalAlphaKernel was added to neurotic in version 1.4.0. neurotic avoids dependence on elephant by subclassing from neurotic._elephant_tools.Kernel instead of elephant.kernels.Kernel. Consequently, neurotic's CausalAlphaKernel is not compatible with elephant.statistics.instantaneous_rate because that function tests that the kernel is a subclass of elephant.kernels.Kernel.
    
    To work around this, here a local version of the class is defined in utils.py which inherits from neurotic and from elephant, allowing utils.CausalAlphaKernel to remain compatible with elephant.statistics.instantaneous_rate.

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -7,34 +7,14 @@ import numpy as np
 import neo
 import elephant
 from pylttb import lttb
+import neurotic
 from neurotic.gui.config import _neo_epoch_to_dataframe
 
-class CausalAlphaKernel(elephant.kernels.AlphaKernel):
-    """
-    This modified version of elephant.kernels.AlphaKernel shifts time such that
-    convolution of the kernel with spike trains (as in
-    elephant.statistics.instantaneous_rate) results in alpha functions that
-    begin rising at the spike time, not before. The entire area of the kernel
-    comes after the spike, rather than half before and half after, as in
-    AlphaKernel. Consequently, CausalAlphaKernel can be used in causal filters.
-
-    Derived from:
-    """
-    __doc__ += elephant.kernels.AlphaKernel.__doc__
-
-    def median_index(self, t):
-        """
-        In CausalAlphaKernel, "median_index" is a misnomer. Instead of returning
-        the index into t that gives half area above and half below (median), it
-        returns the index for the first non-negative time, which always
-        corresponds to the start of the rise phase of the alpha function. This
-        hack ensures that, when the kernel is convolved with a spike train, the
-        entire alpha function is located to the right of each spike time.
-
-        Overrides the following:
-        """
-        return np.nonzero(t >= 0)[0].min()
-    median_index.__doc__ += elephant.kernels.AlphaKernel.median_index.__doc__
+class CausalAlphaKernel(neurotic._elephant_tools.CausalAlphaKernel, elephant.kernels.Kernel):
+    # add elephant.kernels.Kernel as a parent class of CausalAlphaKernel so
+    # that it is usable with elephant.statistics.instantaneous_rate, which
+    # checks that the kernel is a subclass
+    pass
 
 def DownsampleNeoSignal(sig, decimation_factor):
     """
